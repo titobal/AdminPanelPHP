0c55baf482db20615eec4d6ed7b24d469e5e6e0b
Cristobal <cristobal87314712@gmail.com>
2013-05-05 20:33:52 -0300

    Update welco.php

--- admi/welco.php
+++ admi/welco.php
@@ -1,8 +1,8 @@
 <?php
-    /*session_start();
+    if(session_status() != PHP_SESSION_ACTIVE){session_start();}
     if(isset($_SESSION['adm'])){
         header('Location: src/../');
-    }*/
+    }
 ?>
 <!doctype html>
 <html>
