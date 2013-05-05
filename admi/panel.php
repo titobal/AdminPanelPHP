2f7917e67790e16a1fe46d1cd690c274f222f009
Cristobal <cristobal87314712@gmail.com>
2013-05-05 20:34:38 -0300

    Update panel.php

--- admi/panel.php
+++ admi/panel.php
@@ -1,8 +1,8 @@
 <?php
-    /*session_start();
+    if(session_status() != PHP_SESSION_ACTIVE){session_start();}
     if(!isset($_SESSION['adm'])){
         header('Location: src/../');
-    }*/
+    }
     include("../src/php/administrador.class.php");
     $ad = new Administrador();
     $a = $ad->getAdministrador();
